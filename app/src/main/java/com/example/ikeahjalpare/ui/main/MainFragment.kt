d97e165e313d84fd34f0f37c640edee945abeefb
AirPro <AirPro@2016>
2020-07-20 08:38:45 -0400

    Added companion object back into MainFragment.kt

--- app/src/main/java/com/example/ikeahjalpare/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/ikeahjalpare/ui/main/MainFragment.kt
@@ -8,6 +8,7 @@ import android.widget.ArrayAdapter
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
 import com.example.ikeahjalpare.R
 import kotlinx.android.synthetic.main.activity_main.*
@@ -15,11 +16,15 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainFragment : Fragment() {
     private lateinit var viewModel: MainViewModel
 
+    companion object {
+        fun newInstance() = MainFragment
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        ViewModelProviders.of(this).get(MainViewModel:: class.java)
+        ViewModelProvider(this).get(MainViewModel:: class.java)
         val root = inflater.inflate(R.layout.activity_main, container, false)
         val textView:TextView = root.findViewById(R.id.activity_main)
         MainViewModel.text.observe(this.Observer {
@@ -30,7 +35,7 @@ class MainFragment : Fragment() {
     }
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
         viewModel.furnitures.observe(this, Observer {
                 furnitures -> actFurniture.setAdapter(ArrayAdapter(context!!, R.layout.support_simple_spinner_dropdown_item, furnitures))
         })
