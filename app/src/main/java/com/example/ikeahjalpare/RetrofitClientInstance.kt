b366e023b355d0b440b547a492982e6af22650c5
Wesley Reed <reed.wesley.s@gmail.com>
2020-07-23 21:16:39 -0400

    cleaning and make const

--- app/src/main/java/com/example/ikeahjalpare/RetrofitClientInstance.kt
+++ app/src/main/java/com/example/ikeahjalpare/RetrofitClientInstance.kt
@@ -6,18 +6,16 @@ import retrofit2.converter.gson.GsonConverterFactory
 object RetrofitClientInstance {
 
     private var retrofit: Retrofit? = null
-    private val BASE_URL = "https://api.npoint.io"
+    private const val BASE_URL = "https://api.npoint.io"
 
     val retrofitInstance: Retrofit?
         get() {
-            // object has been created
-            if (retrofit == null) { //defined
-                // create it
+            if (retrofit == null) {
                 retrofit = Retrofit.Builder()
-                    .baseUrl(BASE_URL) //base url defined
+                    .baseUrl(BASE_URL)
                     .addConverterFactory(GsonConverterFactory.create())
                     .build()
-            }       //return retrofit
+            }
             return retrofit
         }
 }
